e2760b85b23735bb05cdf92abb505a3cbda6af26
codky <msky0912@gmail.com>
2023-04-21 18:03:52 +0900

    Initial commit

---
+++ src/test/java/com/example/eurekaserverpractice/EurekaServerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaserverpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
